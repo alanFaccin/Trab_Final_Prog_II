d3c84547b6ee54f081c3a374f9720d8f4a24f995
Diovani Bernardi da Motta <diovanimotta@gmail.com>
2015-05-25 21:56:31 -0300

    Commit Parcial
    Funções de alocação dinâmica.

--- Arvore_Genealogica/Arvore_Genealogica/main.c
+++ Arvore_Genealogica/Arvore_Genealogica/main.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define PAI 0;
+#define MAE 1;
+
 //
 //  main.c
 //  Arvore_Genealogica
@@ -5,10 +12,6 @@
 //  Created by Alan Jhones Faccin  Diovani B. da Motta e Rafael Pablo Massocato on 5/25/15.
 //  Copyright (c) 2015 Alan Jhones Faccin Diovani B. da Motta e Rafael Pablo Massocato. All rights reserved.
 //
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 
 struct Pessoa {
 	char* nome;
@@ -26,7 +29,7 @@ struct Pessoa *ultimo;
 ///////////////////////////////////////////////////////////////////
 void inicialize()
 {
-	 struct Pessoa *aux = (struct Pessoa*)(malloc(sizeof(struct Pessoa));
+	 struct Pessoa *aux = (struct Pessoa*)(malloc(sizeof(struct Pessoa)));
 	 primeiro = aux;
 	 ultimo = primeiro;
 	 primeiro->pai = NULL;
@@ -35,15 +38,42 @@ void inicialize()
 
 void inserir(struct Pessoa* pessoa)
 {
-	ultimo->prox = endereco;
-	endereco->ant = ultimo;
-	ultimo = ultimo->prox;
-	endereco->prox = NULL;		
+	ultimo = pessoa;
+	pessoa->pai->mae = NULL;
+	pessoa->pai->pai = NULL;
+	pessoa->mae->pai = NULL;
+	pessoa->mae->mae = NULL;		
+}
+
+void add(struct Pessoa* pessoa,const tipo)
+{
+	switch(tipo)
+	{
+		case PAI:
+			ultimo->pai->pai = pessoa;
+			break;
+		case MAE:
+			ultimo->mae->mae = pessoa;
+			break;
+	}
+	ultimo = pessoa;
+	pessoa->pai->mae = NULL;
+	pessoa->pai->pai = NULL;
+	pessoa->mae->pai = NULL;
+	pessoa->mae->mae = NULL;		
 }
 
 void pesquisar(const char* valor)
 {
 	
+}
+
+int main(int argc, const char * argv[]) {
+    
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+    
 }
 
 void visualizarArvore(struct Pessoa* pessoa)
