c58bdb1ee5958e27b6f2a3605319e778f87fb736
Alan Jhones <alan.fts@gmail.com>
2015-05-25 21:16:38 -0300

    Cabecalho

--- Arvore_Genealogica/Arvore_Genealogica/main.c
+++ Arvore_Genealogica/Arvore_Genealogica/main.c
@@ -2,14 +2,16 @@
 //  main.c
 //  Arvore_Genealogica
 //
-//  Created by Alan Jhones Faccin on 5/25/15.
-//  Copyright (c) 2015 Alan Jhones Faccin. All rights reserved.
+//  Created by Alan Jhones Faccin  Diovani B. da Motta e Rafael Pablo Massocato on 5/25/15.
+//  Copyright (c) 2015 Alan Jhones Faccin Diovani B. da Motta e Rafael Pablo Massocato. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    
     // insert code here...
     printf("Hello, World!\n");
     return 0;
+    
 }
